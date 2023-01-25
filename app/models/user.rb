3823699a159933a79bc36ed3932de75da63d8046
つるつる <84743939+Turu0512@users.noreply.github.com>
2023-01-25 17:00:23 +0900

    modelとフォロー機能などの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,24 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
+  has_many :active_relationships, class_name: 'FollowRelationship', foreign_key: 'follower_id', dependent: :destroy
+  has_many :passive_relationships, class_name: 'FollowRelationship', foreign_key: 'followed_id', dependent: :destroy
+
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships
+
+  # ユーザーをフォローする
+  def follow(other_user)
+    following << other_user
+  end
+
+  # ユーザーをフォロー解除する
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # 現在のユーザーがフォローしてたらtrueを返す
+  def following?(other_user)
+    following.include?(other_user)
+  end
 end
