f55ad5d0411dc82632eb1fbfe45c27299bb74b3c
つるつる <84743939+Turu0512@users.noreply.github.com>
2023-01-26 09:45:32 +0900

    followingをfollowingsに変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,10 +9,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  def following
+  def followings
     @title = 'Following'
     @user  = User.find(params[:id])
-    @users = @user.following.page(params[:page])
+    @users = @user.followings.page(params[:page])
     render 'show_follow'
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,12 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name: 'FollowRelationship', foreign_key: 'follower_id', dependent: :destroy, inverse_of: :follower
   has_many :passive_relationships, class_name: 'FollowRelationship', foreign_key: 'followed_id', dependent: :destroy, inverse_of: :followed
 
-  has_many :following, through: :active_relationships, source: :followed
+  has_many :followings, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships
 
   # ユーザーをフォローする
   def follow(other_user)
-    following << other_user
+    followings << other_user
   end
 
   # ユーザーをフォロー解除する
@@ -23,6 +23,6 @@ class User < ApplicationRecord
 
   # 現在のユーザーがフォローしてたらtrueを返す
   def following?(other_user)
-    following.include?(other_user)
+    followings.include?(other_user)
   end
 end
