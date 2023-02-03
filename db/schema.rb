9997e479cf7726fcc4b0a50e0f9963e8a0464a4e
つるつる <84743939+Turu0512@users.noreply.github.com>
2023-02-03 15:02:51 +0900

    migrationに伴うschemaファイルの変更

--- db/schema.rb
+++ db/schema.rb
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 2023_01_19_072806) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "postal_code"
+    t.string "postal_code"
     t.string "address"
     t.text "profile"
     t.index ["email"], name: "index_users_on_email", unique: true
