6968be4fd686381081fbc192684ef45ca4ad7430
つるつる <84743939+Turu0512@users.noreply.github.com>
2023-02-06 16:41:53 +0900

    フォローボタンの表示方法を変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @follow_relationsihp = current_user.followings.find_by(id: @user.id)
   end
 
   def followings
