6ee3a0f99734c16c1f9910ca1d07b1d96d6c7621
つるつる <84743939+Turu0512@users.noreply.github.com>
2023-02-06 19:15:44 +0900

    不要なファイルを削除

--- app/controllers/users/registrations_controller.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class Users::RegistrationsController < Devise::RegistrationsController
-  def update
-    super
-    resource.avatar.attach(params[:avatar])
-  end
-end
