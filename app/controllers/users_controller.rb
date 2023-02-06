803a41eca38a5ba995c09e8405b4087d7c4ae487
つるつる <84743939+Turu0512@users.noreply.github.com>
2023-02-06 16:42:18 +0900

    user_controllerの共通部分の切り出し

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show followings followers]
   def index
     @users = User.with_attached_avatar.order(:id).page(params[:page])
   end
@@ -11,14 +12,18 @@ class UsersController < ApplicationController
   end
 
   def followings
-    @user  = User.find(params[:id])
     @users = @user.followings.page(params[:page])
     render 'following_list'
   end
 
   def followers
-    @user  = User.find(params[:id])
     @users = @user.followers.with_attached_avatar.page(params[:page])
     render 'followers_list'
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
