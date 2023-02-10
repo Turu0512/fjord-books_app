8a5ef8828a7ad70ed9c4e46b5a2e0b8510c4e08e
つるつる <84743939+Turu0512@users.noreply.github.com>
2023-02-10 09:38:49 +0900

    削除済みのユーザーのコメントの表示名を「退会済み」にする

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,4 +4,14 @@ class Comment < ApplicationRecord
   belongs_to :comment, polymorphic: true
   belongs_to :user
   validates :text, presence: true
+
+  def display_user_name
+    if user.blank?
+      I18n.t('users.show.not_found')
+    elsif user.name.blank?
+      user.email
+    else
+      user.name
+    end
+  end
 end
